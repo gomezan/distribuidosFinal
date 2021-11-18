963d48f2fc00d4780af33cf2928f66fb7c172ccb
Lida Guerrero <tuchysguer@hotmail.com>
2021-11-17 21:06:14 -0500

    Entidad empleador
    Consistencia con tablas en base de datos

--- proyectoDistribuidos/src/main/java/entidad/Empleador.java
+++ proyectoDistribuidos/src/main/java/entidad/Empleador.java
@@ -5,10 +5,47 @@
  */
 package entidad;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author TUCHYS
  */
 public class Empleador {
+    private int codigo;
+    private String razonSocial;
+    private static ArrayList<Sector> listaSector = new ArrayList<Sector>();
+
+    public Empleador() {
+    }
+
+    public Empleador(int codigo, String razonSocial) {
+        this.codigo = codigo;
+        this.razonSocial = razonSocial;
+    }
     
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public static ArrayList<Sector> getListaSector() {
+        return listaSector;
+    }
+
+    public static void setListaSector(ArrayList<Sector> listaSector) {
+        Empleador.listaSector = listaSector;
+    }
+
 }
